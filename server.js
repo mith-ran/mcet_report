ad11f67dc7b8b9e8c17a3a4f8e38db1f66b47707
mith-ran <mithrarmuralitharan@gmail.com>
2025-02-27 00:07:02 +0530

    created server js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const app="ni"
+const exp = require('express');
+const app=exp()
+const port= 8080;
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+
+});
+
+app.listen(port,()=>{
+    console.log(`running at ${port}`)
+})
